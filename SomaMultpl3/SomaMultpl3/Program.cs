57ea1ab21b90f66dcf7a35ad5e2baefcffe78662
joaomln <80040577+joaomln@users.noreply.github.com>
2022-08-17 11:51:59 -0300

    Add files via upload

---
+++ SomaMultpl3/SomaMultpl3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numero = 0;
+
+for(int i = 1; i<=100; i++)
+{
+    if (i % 3 == 0)
+    {
+        
+        Console.WriteLine($"{numero} + {i} = {numero += i}");
+    }
+}
+Console.WriteLine($"\n\n\nValor total: {numero}");
